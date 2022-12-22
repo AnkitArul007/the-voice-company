be82e9d061811d50db4bcf27af756ac77474c757
Ankit <nktarul@gmail.com>
2022-12-22 14:41:18 +0530

    commiting final changes with components

--- src/pages/internship.js
+++ src/pages/internship.js
@@ -3,13 +3,11 @@ import Layout from "../components/Layout";
 import Button from "../components/Button";
 import Cohort from "../components/Cohort";
 import CopywritingBasics from "../components/CopywritingBasics";
+import Navbar from "../components/Navbar";
 
 export default function Internship() {
   return (
     <Layout>
-      <h1>This is the internship page</h1>
-      <Button></Button>
-      <Cohort></Cohort>
       <CopywritingBasics></CopywritingBasics>
     </Layout>
   );
