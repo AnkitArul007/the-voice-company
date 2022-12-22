b7aeb45911ceb081aa776efecb4cef8738394656
phoenix <shivashishbhaskar@gmail.com>
2022-12-22 16:30:04 +0530

    deployment changes

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,15 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\shiva\\Desktop\\trd projects\\the-voice-company\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("C:\\Users\\shiva\\Desktop\\trd projects\\the-voice-company\\src\\pages\\404.js")),
+  "component---src-pages-about-js": preferDefault(require("C:\\Users\\shiva\\Desktop\\trd projects\\the-voice-company\\src\\pages\\about.js")),
+  "component---src-pages-classes-js": preferDefault(require("C:\\Users\\shiva\\Desktop\\trd projects\\the-voice-company\\src\\pages\\classes.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\shiva\\Desktop\\trd projects\\the-voice-company\\src\\pages\\index.js")),
+  "component---src-pages-internship-js": preferDefault(require("C:\\Users\\shiva\\Desktop\\trd projects\\the-voice-company\\src\\pages\\internship.js")),
+  "component---src-pages-review-js": preferDefault(require("C:\\Users\\shiva\\Desktop\\trd projects\\the-voice-company\\src\\pages\\review.js"))
+}
+
