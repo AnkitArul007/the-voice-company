42f2117d280d290dc83f8cfed3e4715f3677ad24
phoenix <shivashishbhaskar@gmail.com>
2022-12-22 13:30:23 +0530

    classes page completed

--- src/components/Cohort.js
+++ src/components/Cohort.js
@@ -1,23 +1,17 @@
-import React from 'react';
+import React from "react";
 import "../styles/cohort.scss";
 
 function Cohort() {
   return (
-    <section className="cohortSection">
-        <div className="cohortNumCont">
-
-        </div>
-        <div className="cohortHeadlinesCont">
-            <h2 className="cohortHeadline">
-                Next cohort starts on 
-            </h2>
-            <h2 className='cohortHeadlineSecond'> 15 Dec 2022.</h2>
-            <h2 className='cohortHeadlineThird'>
-                5 seats left
-            </h2>
-        </div>
+    <section className="cohortSection section-wrapper">
+      <div className="cohortNumCont"></div>
+      <div className="cohortHeadlinesCont">
+        <h2 className="cohortHeadline">Next cohort starts on</h2>
+        <h2 className="cohortHeadlineSecond"> 15 Dec 2022.</h2>
+        <h2 className="cohortHeadlineThird">5 seats left</h2>
+      </div>
     </section>
-  )
+  );
 }
 
 export default Cohort;
